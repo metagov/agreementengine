c7bf5b2ff54f8be742aa2f46599ec788cef1ab0b
Luke Miller <millerluke1364@gmail.com>
2021-07-16 11:24:29 -0400

    keeping prototype out of main branch

--- agreementengine/__init__.py
+++ agreementengine/__init__.py
@@ -1,15 +1,28 @@
 class AgreementPath:
     def __init__(self) -> None:
-        self.states = []
+        self.states = [
+            Registration()
+        ]
 
 
 class State:
-    def __init__(self) -> None:
+    def __init__(self):
         pass
 
-    def begin():
+    def begin(self):
         pass
 
-    def end():
+    def end(self):
         pass
 
+class Registration(State):
+    def __init__(self):
+        super().__init__()
+
+class NDARegistration(Registration):
+    def __init__(self):
+        super().__init__()
+
+new_state = State()
+
+State.begin(new_state)
