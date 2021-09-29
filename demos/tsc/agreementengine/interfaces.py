45dd667e394472cf4ef86f8fcbeb0b85cbe7bc90
Luke Miller <millerluke1364@gmail.com>
2021-09-28 20:33:53 -0400

    lots of improvements, agreement matching for follow up threads

--- demos/tsc/agreementengine/interfaces.py
+++ demos/tsc/agreementengine/interfaces.py
@@ -1,10 +1,21 @@
 from agreementengine.interface import Interface
+from tinydb import Query
 
 class TwitterInterface(Interface):
     def filter(self):
         return True
 
     def match(self):
-        text = self.request.json['data']['full_text']
+        text = self.request.json['full_text'][len('@agreementengine '):]
         
-        return {}
+        if 'agreement' in text:
+            return {
+                'id': self.request.json['id_str']
+            }
+
+        elif 'broken' in text:
+            print(self.request.json['in_reply_to_status_id_str'])
+            return Query()['agreement']['TwitterInterface']['id'] == self.request.json['in_reply_to_status_id_str']
+            
+        elif 'upheld' in text:
+            return Query()['agreement'][self.name]['id'] == self.request.json['in_reply_to_status_id_str']
--- demos/tsc/agreementengine/path.py
+++ demos/tsc/agreementengine/path.py
@@ -9,7 +9,19 @@ class TSC(AgreementPath):
 
     class Authoring(AgreementProcess):
         def on_receive(self, data):
-            print(data)
+            self.model.set('data', 'creator_id', data['user']['id_str'])
+            try:
+                self.model.set('data', 'member_id', data['entities']['user_mentions'][1]['id_str'])
+            except IndexError:
+                print('Agreement missing other user')
+                self.path.terminate()
+                return
+
+            self.path.transition_to(TSC.Registration)
+
+    class Registration(AgreementProcess):
+        def on_receive(self, data):
+            ...
     
     interfaces = [
         TwitterInterface
